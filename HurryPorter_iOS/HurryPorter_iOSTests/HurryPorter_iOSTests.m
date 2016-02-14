5c6e5b824377f3b8986538f05d678fa6cf4643e4
Seachaos <ye.jian.yin@gmail.com>
2016-02-14 18:28:24 +0800

    can be test

--- HurryPorter_iOS/HurryPorter_iOSTests/HurryPorter_iOSTests.m
+++ HurryPorter_iOS/HurryPorter_iOSTests/HurryPorter_iOSTests.m
@@ -66,7 +66,7 @@
     XCTAssertTrue([testDict isEqual:exceptDict]);
     exceptDict = @{@"BB":@"AA"};
     XCTAssertFalse([testDict isEqual:exceptDict]);
-    exceptDict = @{@"AA":@"BcB"};
+    exceptDict = @{@"AA":@"BB"};
     XCTAssertTrue([testDict isEqual:exceptDict]);
 }
 
