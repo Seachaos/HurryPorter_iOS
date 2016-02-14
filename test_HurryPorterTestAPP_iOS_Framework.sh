2458c8118d7960a4765c538e6d55d84d80e455c2
Seachaos <ye.jian.yin@gmail.com>
2016-02-14 19:08:20 +0800

    try for ci build

--- test_HurryPorterTestAPP_iOS_Framework.sh
+++ test_HurryPorterTestAPP_iOS_Framework.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ev
-./build_iOSFramework.sh
+# ./build_iOSFramework.sh
 
 cd ./HurryPorterTestAPP_iOS
 xctool clean -sdk iphonesimulator -project HurryPorterTestAPP.xcodeproj -scheme HurryPorterTestAPP build test
