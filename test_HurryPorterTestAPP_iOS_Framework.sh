14b271e439d19035187dc39497469ec7882c0aa2
Seachaos <ye.jian.yin@gmail.com>
2016-02-14 18:53:35 +0800

    try use framework for test

--- test_HurryPorterTestAPP_iOS_Framework.sh
+++ test_HurryPorterTestAPP_iOS_Framework.sh
@@ -2,13 +2,13 @@
 set -ev
 
 # build framework
-# cd HurryPorter_iOS
-# xctool clean -sdk iphonesimulator -project HurryPorter_iOS.xcodeproj -scheme BuildFramework build
-# cd ../
+cd HurryPorter_iOS
+xctool clean -sdk iphonesimulator -project HurryPorter_iOS.xcodeproj -scheme BuildFramework build
+cd ../
 
 
 # copy framework
-# rm -rf ./HurryPorter_iOS/HurryPorterTestAPP/HurryPorter_iOS.framework
-# cp HurryPorter_iOS/Output/HurryPorter_iOS-Debug-iphoneuniversal/HurryPorter_iOS.framework ./HurryPorter_iOS/HurryPorterTestAPP
+rm -rf ./HurryPorter_iOS/HurryPorterTestAPP/HurryPorter_iOS.framework
+cp HurryPorter_iOS/Output/HurryPorter_iOS-Debug-iphoneuniversal/HurryPorter_iOS.framework ./HurryPorter_iOS/HurryPorterTestAPP
 
 xctool clean -sdk iphonesimulator -workspace HurryPorter.xcworkspace -scheme HurryPorterTestAPP build test
