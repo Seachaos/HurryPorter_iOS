da6d6d5a95e3d8378fd012cfd3c56ffcc6333b06
Seachaos <ye.jian.yin@gmail.com>
2016-02-14 18:46:00 +0800

    try test

--- test_HurryPorterTestAPP_iOS_Framework.sh
+++ test_HurryPorterTestAPP_iOS_Framework.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 set -ev
 
-# xctool clean -sdk iphonesimulator -workspace HurryPorter.xcworkspace -scheme HurryPorterTestAPP build test
+# build framework
+# cd HurryPorter_iOS
+# xctool clean -sdk iphonesimulator -project HurryPorter_iOS.xcodeproj -scheme BuildFramework build
+# cd ../
+
+
+# copy framework
+# rm -rf ./HurryPorter_iOS/HurryPorterTestAPP/HurryPorter_iOS.framework
+# cp HurryPorter_iOS/Output/HurryPorter_iOS-Debug-iphoneuniversal/HurryPorter_iOS.framework ./HurryPorter_iOS/HurryPorterTestAPP
+
+xctool clean -sdk iphonesimulator -workspace HurryPorter.xcworkspace -scheme HurryPorterTestAPP build test
